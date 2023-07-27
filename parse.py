71b2ceca315bc8e6f8012363f0082e4a4a873aac
Ivan <ivanulanovskij143@gmail.com>
2023-07-27 12:38:30 +0500

    поставлен break для экономии времени (dev)

--- parse.py
+++ parse.py
@@ -22,6 +22,7 @@ def get_houses_urls () -> list:
                 houses_urls.append(link.attrs["href"])
             else:
                 continue
+        break # Зздесь break чтобы не тратить много времени на парсинг, после окончания работы - убрать
         
         page+=1
 
