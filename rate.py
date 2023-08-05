bb7dafd17d4f2b0bdc0d7db620544fa3d58d76da
Ivan <ivanulanovskij143@gmail.com>
2023-08-05 19:45:14 +0500

    убраны функции-оценщики алгоритмов нормализации

--- rate.py
+++ rate.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -14,84 +13,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop, Adam
 from keras.metrics import MeanSquaredError
-
-
-# Оценка скалеров
-def rate_scalers (scalers: dict, X: pd.DataFrame) -> dict:
-    result = {}
-    intermediate_score_scaler = []
-
-    for name in scalers.keys():
-        result[name] = []
-
-    for name, scaler in scalers.items():
-        
-        intermediate_score_scaler = []
-
-        working_scaler = scaler().fit(X)
-        intermediate_X = pd.DataFrame(data=working_scaler.transform(X), columns=X.columns)
-
-        for column in intermediate_X.columns:
-            intermediate_score_scaler.append({f"min[{column}]": min(intermediate_X[column]), f"max[{column}]": max(intermediate_X[column])})
-        
-        score_scaler = result[name].copy()
-        score_scaler.append({"MIN_MAX": intermediate_score_scaler})
-        result[name] = score_scaler.copy()
-
-        intermediate_score_scaler = []
-        score_scaler = []
-
-        for column in intermediate_X.columns:
-            intermediate_score_scaler.append({f"mean[{column}]": np.mean(intermediate_X[column]), f"std[{column}]": np.std(intermediate_X[column])})
-
-        score_scaler = result[name].copy()
-        score_scaler.append({"MEAN_STD": intermediate_score_scaler})
-        result[name] = score_scaler.copy()
-
-        intermediate_score_scaler = []
-        score_scaler = []
-
-    return result
-
-def plot_scaler_data (scaler_score: dict, data_type: str, scaler_name: str, X: pd.DataFrame, fchart: str="scalers_charts/"):
-    DATA = scaler_score[data_type]
-
-    chart_data_full = []
-    chart_data_1 = []
-    chart_data_2 = []
-
-    for index,column in enumerate(X.columns):
-        chart_data_1.append(DATA[index][f"{data_type.lower().split('_')[0]}[{column}]"])
-        chart_data_2.append(DATA[index][f"{data_type.lower().split('_')[1]}[{column}]"])
-
-    chart_data_full.append(chart_data_1)
-    chart_data_full.append(chart_data_2)
-
-    chart_data_1 = []
-    chart_data_2 = []
-    
-    sns.set_style("darkgrid")
-
-    plt.figure(figsize=(10, 6))
-
-    for i, data_row in enumerate(chart_data_full):
-        plt.barh(X.columns, data_row, label=f"{data_type.lower().split('_')[i]}", alpha=0.5)   
-
-    plt.xlabel("Признаки")
-    plt.ylabel("Значения")
-    plt.title(f"{data_type} - {scaler_name}")
-    plt.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig(f"{fchart}/{data_type} - {scaler_name}")
-
-def create_scaler_charts (score_scalers: dict, X: pd.DataFrame):
-    for scaler_name, scaler_score_data in score_scalers.items():
-        for scaler_score in scaler_score_data:
-            if "MIN_MAX" in scaler_score:
-                plot_scaler_data(scaler_score, "MIN_MAX", scaler_name, X)
-            else:
-                plot_scaler_data(scaler_score, "MEAN_STD", scaler_name, X)
 
 
 def create_model (input_shape: int) -> keras.Model:
