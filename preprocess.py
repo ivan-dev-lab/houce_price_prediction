9306179feafe748061af7f4c0af92415f97161ab
Ivan <ivanulanovskij143@gmail.com>
2023-08-06 12:48:57 +0500

    внесены изменения в алгоритм обработки данных

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import numpy as np
 
 ## \brief Функция предобработки данных 
 ## \authors ivan-dev-lab
@@ -12,39 +13,37 @@ def preprocess (fpath: str) -> dict:
 
     data = pd.read_csv(fpath)
 
+    data.drop_duplicates(inplace=True)
+    data["price"].replace(0, np.nan, inplace=True)
+    data.dropna(inplace=True)
+
     data_preprocessed_dict = {
         "data": pd.DataFrame,
         "X": pd.DataFrame,
         "Y": pd.DataFrame,
     }
-
     
     data.drop("date", axis=1, inplace=True)
     data.drop("street", axis=1, inplace=True)
     data.drop("statezip", axis=1, inplace=True)
     data.drop("country", axis=1, inplace=True)
-    data.drop("sqft_basement", axis=1, inplace=True)
+    data.drop("sqft_above", axis=1, inplace=True)
 
-    city_replacements = {}
-
-    for unique_index,unique_city in enumerate(data["city"].unique()):
-        city_replacements[unique_city] = unique_index
-
-    data["city"].replace(city_replacements, inplace=True)
+    data = pd.get_dummies(data, columns=["city"], prefix=["city"])
     data_columns = data.columns
 
     Y = data["price"]
 
-    scaler = StandardScaler().fit(data)
-    data = pd.DataFrame(data=scaler.transform(data), columns=data_columns)
+    scaler = StandardScaler()
+    data = pd.DataFrame(data=scaler.fit_transform(data), columns=data_columns)
 
-    X = data.iloc[:, 1:]
+    X = data[['bedrooms','bathrooms','sqft_living','sqft_lot']]
+
 
     data_preprocessed_dict["data"] = data
     data_preprocessed_dict["X"] = X
     data_preprocessed_dict["Y"] = Y
 
-    print(data_preprocessed_dict)
-
     return data_preprocessed_dict
     
+preprocess("data/houses-data_kaggle.csv")
