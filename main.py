134274b090e8c9db8d9df029ded0d474ae3ef686
Ivan <ivanulanovskij143@gmail.com>
2023-08-09 09:39:44 +0500

    исправлены мелкие ошибки

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import KFold, train_test_split
-from keras import callbacks
+from keras import Model
 from preprocess import preprocess
 from rate import rate_models, create_models_charts
 from create_model import create_model
@@ -48,19 +48,18 @@ def main ():
     ## \authors ivan-dev-lab
     ## \version 1.0.0
     ## \date 06.08.2023
+    ## \param[out] model Параметр принимает созданную и скомпилированную модель, необходимую для дальнейшего обучения
     ## \param[in] x_train Одна из частей данных для тренировки модели. Содержит в себе 80% признаков из данных
     ## \param[in] x_test Одна из частей данных для тренировки модели. Содержит в себе 20% признаков из данных
     ## \param[in] y_train Одна из частей данных для тренировки модели. Содержит в себе 80% целевых переменных из данных
     ## \param[in] y_test Одна из частей данных для тренировки модели. Содержит в себе 20% целевых переменных из данных
-    ## \return Словарь dict с такими метриками модели, как mse, mae, r2_score
-    def train_model (x_train, x_test, y_train, y_test) -> dict:
+    ## \return Словарь dict с такими метриками модели, как mse, mae, r2_score    
+    def train_model (model: Model, x_train: pd.DataFrame, x_test: pd.DataFrame, y_train: pd.DataFrame, y_test: pd.DataFrame) -> dict:
         scores_dict = {
             "mse": float,
             "mae": float,
             "r2_score": float
         }
-
-        model = create_model(x_train.shape[1])
 
         model.fit(x_train, y_train, batch_size=64, epochs=30, verbose=0)
         y_pred = model.predict(x_test)
@@ -71,10 +70,13 @@ def main ():
 
         return scores_dict
 
-    train_result = train_model (x_train, x_test, y_train, y_test)
+    model = create_model(x_train.shape[1])
+
+    # print(model.get_config())
+
+    train_result = train_model (model, x_train, x_test, y_train, y_test)
 
     print(f"Итоговые результаты модели:\nmse = {train_result['mse']}\nmae = {train_result['mae']}\nr2_score = {train_result['r2_score']}")
-
 
 if __name__ == "__main__":
     main()
