d8b43308752f750438a9abd68838961c14bf33e9
Ivan <ivanulanovskij143@gmail.com>
2023-08-13 21:58:53 +0500

    файлы с обновленной документацией

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['get_5fhouse_5fparameters_0',['get_house_parameters',['../namespaceparse.html#a5f075fe82e7b518e502b07d9adcc690d',1,'parse']]],
-  ['get_5fhouses_5furls_1',['get_houses_urls',['../namespaceparse.html#a67ec5d5b4e99b11609398c0327db1e99',1,'parse']]]
+  ['get_5fbest_5fmodels_0',['get_best_models',['../namespacerate.html#acc01d01b208b1af5fd46ca4986cb71ec',1,'rate']]],
+  ['get_5fhouse_5fparameters_1',['get_house_parameters',['../namespaceparse.html#a5f075fe82e7b518e502b07d9adcc690d',1,'parse']]],
+  ['get_5fhouses_5furls_2',['get_houses_urls',['../namespaceparse.html#a67ec5d5b4e99b11609398c0327db1e99',1,'parse']]]
 ];
